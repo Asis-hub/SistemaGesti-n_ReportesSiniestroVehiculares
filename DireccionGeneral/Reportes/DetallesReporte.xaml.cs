67288e4e18fda5f90687f258e4b93820bc4ffb20
midguet12 <midguet12@hotmail.com>
2021-04-22 12:23:56 -0500

    agregar elementos a ventana

--- DireccionGeneral/Reportes/DetallesReporte.xaml.cs
+++ DireccionGeneral/Reportes/DetallesReporte.xaml.cs
@@ -23,5 +23,10 @@ namespace DireccionGeneral.Reportes
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
