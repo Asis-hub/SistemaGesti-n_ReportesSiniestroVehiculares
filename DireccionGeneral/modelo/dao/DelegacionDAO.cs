1074b690731c4970572d959892797ee5eea09efb
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2021-05-22 21:28:40 -0500

    CRUD Delegacion Terminado, faltan notificaciones MessageBox

--- DireccionGeneral/modelo/dao/DelegacionDAO.cs
+++ DireccionGeneral/modelo/dao/DelegacionDAO.cs
@@ -56,8 +56,6 @@ namespace DireccionGeneral.modelo.dao
 
             mensaje = JsonSerializer.Serialize(paquete);
 
-            Console.WriteLine("Mensaje DAO: " + mensaje);
-
             socket.IniciarConexion();
             socket.EnviarMensaje(mensaje);
             string respuesta = socket.RecibirMensaje();
@@ -68,6 +66,84 @@ namespace DireccionGeneral.modelo.dao
                 listaDelegaciones = (List<Delegacion>)JsonSerializer.Deserialize(respuesta, typeof(List<Delegacion>)); ;
             }
             return listaDelegaciones;
+        }
+
+        public static int RegistrarDelegacion(Delegacion delegacion)
+        {
+            int resultado = 0;
+            SocketBD socket = new SocketBD();
+            Paquete paquete = new Paquete();
+            paquete.TipoQuery = TipoConsulta.Insert;
+            paquete.TipoDominio = TipoDato.Delegacion;
+            paquete.Consulta = String.Format("INSERT INTO dbo.delegacion (nombre, correo, codigoPostal, calle, colonia, numero, tipo, idMunicipio) " +
+                                             "VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', {6}, {7})",
+                                             delegacion.Nombre, delegacion.Correo, delegacion.CodigoPostal, delegacion.Calle, delegacion.Colonia, 
+                                             delegacion.Numero, delegacion.IdTipo, delegacion.IdMunicipio);
+            Console.WriteLine(paquete.Consulta);
+            string mensaje = JsonSerializer.Serialize(paquete);
+
+            socket.IniciarConexion();
+            socket.EnviarMensaje(mensaje);
+            string respuesta = socket.RecibirMensaje();
+            socket.TerminarConexion();
+
+            if (respuesta.Length > 0)
+            {
+                resultado = int.Parse(respuesta);
+            }
+            
+            return resultado;
+        }
+
+        public static int EditarDelegacion(Delegacion delegacion)
+        {
+            int resultado = 0;
+            SocketBD socket = new SocketBD();
+            Paquete paquete = new Paquete();
+            paquete.TipoQuery = TipoConsulta.Update;
+            paquete.TipoDominio = TipoDato.Delegacion;
+            paquete.Consulta = String.Format("UPDATE dbo.delegacion SET nombre='{0}', codigoPostal='{1}', correo='{2}', calle='{3}', " +
+                                             "colonia='{4}', numero='{5}', tipo={6}, idMunicipio={7} WHERE idDelegacion={8}",
+                                             delegacion.Nombre, delegacion.CodigoPostal, delegacion.Correo, delegacion.Calle,
+                                             delegacion.Colonia, delegacion.Numero, delegacion.IdTipo, delegacion.IdMunicipio, delegacion.IdDelegacion);
+
+            string mensaje = JsonSerializer.Serialize(paquete);
+
+            socket.IniciarConexion();
+            socket.EnviarMensaje(mensaje);
+            string respuesta = socket.RecibirMensaje();
+            socket.TerminarConexion();
+
+            if (respuesta.Length > 0)
+            {
+                resultado = int.Parse(respuesta);
+            }
+
+            return resultado;
+        }
+
+        public static int EliminarDelegacion(int idDelegacion)
+        {
+            int resultado = 0;
+            SocketBD socket = new SocketBD();
+            Paquete paquete = new Paquete();
+            paquete.TipoQuery = TipoConsulta.Delete;
+            paquete.TipoDominio = TipoDato.Delegacion;
+            paquete.Consulta = String.Format("DELETE FROM dbo.delegacion WHERE idDelegacion={0}", idDelegacion);
+
+            string mensaje = JsonSerializer.Serialize(paquete);
+
+            socket.IniciarConexion();
+            socket.EnviarMensaje(mensaje);
+            string respuesta = socket.RecibirMensaje();
+            socket.TerminarConexion();
+
+            if (respuesta.Length > 0)
+            {
+                resultado = int.Parse(respuesta);
+            }
+
+            return resultado;
         }
     }
 }
