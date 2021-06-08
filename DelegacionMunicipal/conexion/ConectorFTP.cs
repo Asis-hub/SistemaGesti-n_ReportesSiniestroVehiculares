b91ec7850d8a897c4edcf2e46c17432bb3afad03
midguet12 <midguet12@hotmail.com>
2021-06-07 21:06:15 -0500

    metodo que retorna imagen

--- DelegacionMunicipal/conexion/ConectorFTP.cs
+++ DelegacionMunicipal/conexion/ConectorFTP.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace DelegacionMunicipal.conexion
 {
@@ -11,11 +13,27 @@ namespace DelegacionMunicipal.conexion
 
     class ConectorFTP
     {
-        string servidor = "ftp://maisonbleue2020.ddns.net/";
-        WebClient clienteWeb = new WebClient();
-        NetworkCredential credenciales = new NetworkCredential("pi", "raspberry");
+        
+        
 
+        public static BitmapImage obtenerConexion(string name)
+        {
+            string servidor = "ftp://maisonbleue2020.ddns.net/";
+            WebClient clienteWeb = new WebClient();
+            NetworkCredential credenciales = new NetworkCredential("pi", "raspberry");
+            clienteWeb.Credentials = credenciales;
 
+            MemoryStream memoria = new MemoryStream(clienteWeb.DownloadData(servidor + name));
+
+            BitmapImage imagen = new BitmapImage();
+            imagen.BeginInit();
+            imagen.StreamSource = memoria;
+            imagen.EndInit();
+
+            return imagen;
+            
+
+        }
 
 
     }
