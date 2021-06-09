1bf340bdafb61903dc5c565683de6c85abdff542
midguet12 <midguet12@hotmail.com>
2021-06-09 13:40:50 -0500

    FotografiaDAO

--- Servidor/modelo/dao/FotografiaDAO.cs
+++ Servidor/modelo/dao/FotografiaDAO.cs
@@ -1,6 +1,8 @@
-﻿using Servidor.modelo.poco;
+﻿using Servidor.modelo.dao.db;
+using Servidor.modelo.poco;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +12,47 @@ namespace Servidor.modelo.dao
     class FotografiaDAO
     {
 
-        public static List<Fotografia> ObtenerFotografias(int id)
+        public static List<Fotografia> ObtenerFotografias(string consulta)
         {
-            return 
+            List<Fotografia> fotografias = new List<Fotografia>;
+
+            SqlConnection conn = null;
+
+            try
+            {
+                conn = ConexionBD.GetConnection();
+                if (conn != null)
+                {
+                    SqlCommand comando;
+                    SqlDataReader dataReader;
+                    comando = new SqlCommand(consulta, conn);
+                    dataReader = comando.ExecuteReader();
+
+                    while (dataReader.Read())
+                    {
+                        Fotografia fotografia = new Fotografia();
+                        fotografia.IdFotografia = (!dataReader.IsDBNull(0)) ? dataReader.GetInt32(0) : 0;
+                        fotografia.IdReporte = (!dataReader.IsDBNull(1)) ? dataReader.GetInt32(1) : 0;
+                        fotografias.Add(fotografia);
+                    }
+                    dataReader.Close();
+                    comando.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+
+
+            return fotografias;
         }
     }
 }
