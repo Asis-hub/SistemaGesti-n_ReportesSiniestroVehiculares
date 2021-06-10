c62e2a2feb05a7a21b14affc376c64690a2c1d1c
midguet12 <midguet12@hotmail.com>
2021-06-10 13:57:27 -0500

    Excepcion para cuando no hay foto

--- DelegacionMunicipal/vistas/DetallesReporte.xaml.cs
+++ DelegacionMunicipal/vistas/DetallesReporte.xaml.cs
@@ -27,17 +27,19 @@ namespace DelegacionMunicipal.vistas
             lbl_Delegacion.Content = reporteSiniestro.IdDelegacion.ToString();
             lbl_Usuario.Content = reporteSiniestro.Username.ToString();
 
-            List<Fotografia> fotografias = FotografiaDAO.ObtenerFotografias(idReporte);
+            cargarFotos(idReporte);
+            
 
+            
+            
 
-            img_imagen1.Source = ConectorFTP.obtenerImagen(fotografias[0].IdFotografia.ToString());
-            img_imagen2.Source = ConectorFTP.obtenerImagen(fotografias[1].IdFotografia.ToString());
+            /*img_imagen2.Source = ConectorFTP.obtenerImagen(fotografias[1].IdFotografia.ToString());
             img_imagen3.Source = ConectorFTP.obtenerImagen(fotografias[2].IdFotografia.ToString());
             img_imagen4.Source = ConectorFTP.obtenerImagen(fotografias[3].IdFotografia.ToString());
             img_imagen5.Source = ConectorFTP.obtenerImagen(fotografias[4].IdFotografia.ToString());
             img_imagen6.Source = ConectorFTP.obtenerImagen(fotografias[5].IdFotografia.ToString());
             img_imagen7.Source = ConectorFTP.obtenerImagen(fotografias[6].IdFotografia.ToString());
-            img_imagen8.Source = ConectorFTP.obtenerImagen(fotografias[7].IdFotografia.ToString());
+            img_imagen8.Source = ConectorFTP.obtenerImagen(fotografias[7].IdFotografia.ToString());*/
 
             if (reporteSiniestro.Dictamen)
             {
@@ -58,6 +60,100 @@ namespace DelegacionMunicipal.vistas
 
 
         }
+
+        private void cargarFotos(int idReporte)
+        {
+            List<Fotografia> fotografias = FotografiaDAO.ObtenerFotografias(idReporte);
+
+            //Imagen 1
+            try
+            {
+                img_imagen1.Source = ConectorFTP.obtenerImagen(fotografias[0].IdFotografia.ToString());
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+
+                img_imagen1.Source = ConectorFTP.obtenerImagen("NF");
+            }
+
+            //Imagen 2
+            try
+            {
+                img_imagen2.Source = ConectorFTP.obtenerImagen(fotografias[1].IdFotografia.ToString());
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+
+                img_imagen2.Source = ConectorFTP.obtenerImagen("NF");
+            }
+
+            //Imagen 3
+            try
+            {
+                img_imagen3.Source = ConectorFTP.obtenerImagen(fotografias[2].IdFotografia.ToString());
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+
+                img_imagen3.Source = ConectorFTP.obtenerImagen("NF");
+            }
+
+            //Imagen 4
+            try
+            {
+                img_imagen4.Source = ConectorFTP.obtenerImagen(fotografias[3].IdFotografia.ToString());
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+
+                img_imagen4.Source = ConectorFTP.obtenerImagen("NF");
+            }
+
+            //Imagen 5
+            try
+            {
+                img_imagen5.Source = ConectorFTP.obtenerImagen(fotografias[4].IdFotografia.ToString());
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+
+                img_imagen5.Source = ConectorFTP.obtenerImagen("NF");
+            }
+
+            //Imagen 6
+            try
+            {
+                img_imagen6.Source = ConectorFTP.obtenerImagen(fotografias[5].IdFotografia.ToString());
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+
+                img_imagen6.Source = ConectorFTP.obtenerImagen("NF");
+            }
+
+            //Imagen 7
+            try
+            {
+                img_imagen7.Source = ConectorFTP.obtenerImagen(fotografias[6].IdFotografia.ToString());
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+
+                img_imagen7.Source = ConectorFTP.obtenerImagen("NF");
+            }
+
+            //Imagen 8
+            try
+            {
+                img_imagen8.Source = ConectorFTP.obtenerImagen(fotografias[7].IdFotografia.ToString());
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+
+                img_imagen8.Source = ConectorFTP.obtenerImagen("NF");
+            }
+        }
+
 
         
     }
