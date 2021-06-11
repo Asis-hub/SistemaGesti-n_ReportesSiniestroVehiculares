012ba33fa87298a08c31721e94f4c5350a306917
midguet12 <midguet12@hotmail.com>
2021-06-11 06:12:42 -0500

    Poco vehiculosInvolucrados

---
+++ DelegacionMunicipal/modelo/poco/VehiculosInvolucrados.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegacionMunicipal.modelo.poco
+{
+    class VehiculosInvolucrados
+    {
+        private string numeroPlaca;
+        private int idReporte;
+
+        public string NumeroPlaca { get => numeroPlaca; set => numeroPlaca = value; }
+
+        public int IdReporte { get => idReporte; set => idReporte = value; }
+
+    }
+}
